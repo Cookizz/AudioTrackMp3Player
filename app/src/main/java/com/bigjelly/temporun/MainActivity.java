4918eb525520280e3d839c16ff2b0312f840115c
Cookizz <cookizz@qq.com>
2015-09-21 13:56:48 +0800

    rectify the sample mp3 file url, add onDestroy() protection.

--- app/src/main/java/com/bigjelly/temporun/MainActivity.java
+++ app/src/main/java/com/bigjelly/temporun/MainActivity.java
@@ -5,14 +5,11 @@ import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.os.Bundle;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
 import javazoom.jl.decoder.Bitstream;
-import javazoom.jl.decoder.BitstreamException;
 import javazoom.jl.decoder.Decoder;
-import javazoom.jl.decoder.DecoderException;
 import javazoom.jl.decoder.Header;
 import javazoom.jl.decoder.SampleBuffer;
 
@@ -20,6 +17,7 @@ import javazoom.jl.decoder.SampleBuffer;
 public class MainActivity extends Activity {
 
     private Decoder mDecoder;
+    private AudioTrack mAudioTrack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +29,7 @@ public class MainActivity extends Activity {
                 AudioFormat.CHANNEL_OUT_STEREO,
                 AudioFormat.ENCODING_PCM_16BIT);
 
-        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+        mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
                 sampleRate,
                 AudioFormat.CHANNEL_OUT_STEREO,
                 AudioFormat.ENCODING_PCM_16BIT,
@@ -43,7 +41,7 @@ public class MainActivity extends Activity {
             @Override
             public void run() {
                 try {
-                    InputStream in = new URL("http://218.5.238.156/ws.cdn.baidupcs.com/file/a4faafe8eb86caf35b56d636a9d42c50?bkt=p2-qd-314&xcode=8defc358365529893b7d0db111a577918ad33029379d3321a271ffda32bcb45b&fid=4283643989-250528-583541549892735&time=1441517713&sign=FDTAXGERLBH-DCb740ccc5511e5e8fedcff06b081203-PthAkjXBTlf38k5%2BbSMD1ca7z8w%3D&to=lc&fm=Nan,B,T,t&sta_dx=3&sta_cs=1689&sta_ft=mp3&sta_ct=5&fm2=Nanjing,B,T,t&newver=1&newfm=1&secfm=1&flow_ver=3&pkey=1400a4faafe8eb86caf35b56d636a9d42c5012b1193d0000003789a4&sl=69599310&expires=8h&rt=sh&r=719768603&mlogid=2636057181&vuk=-&vbdid=622005031&fin=Nothing%27s%20Gonna%20Change%20My%20Love%20For%20You.mp3&fn=Nothing%27s%20Gonna%20Change%20My%20Love%20For%20You.mp3&slt=pm&uta=0&rtype=1&iv=0&isw=0&wshc_tag=0&wsts_tag=55ebd093&wsid_tag=3b2466f7&wsiphost=ipdbm")
+                    InputStream in = new URL("http://icecast.omroep.nl:80/radio1-sb-mp3")
                             .openConnection()
                             .getInputStream();
                     Bitstream bitstream = new Bitstream(in);
@@ -55,21 +53,24 @@ public class MainActivity extends Activity {
                     for(; framesReaded-- > 0 && (header = bitstream.readFrame()) != null;) {
                         SampleBuffer sampleBuffer = (SampleBuffer) mDecoder.decodeFrame(header, bitstream);
                         short[] buffer = sampleBuffer.getBuffer();
-                        audioTrack.write(buffer, 0, buffer.length);
+                        mAudioTrack.write(buffer, 0, buffer.length);
                         bitstream.closeFrame();
                     }
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (BitstreamException e) {
-                    e.printStackTrace();
-                } catch (DecoderException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
         thread.start();
 
-        audioTrack.play();
+        mAudioTrack.play();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mAudioTrack.stop();
     }
 }
